f43d92deea5fb496461cec40a96f109eaae76de1
Derek Babb <dbabb1@gmail.com>
2020-02-02 21:48:18 -0600

    Shell code for lab 4.

---
+++ TurtleGraphics.py
@@ -0,0 +1,27 @@
+#TurtleGraphics.py
+#Name:
+#Date:
+#Assignment:
+
+import turtle
+
+def drawSquare(myTurtle, size):
+    for i in range(4):
+        myTurtle.forward(size)
+        myTurtle.right(90)
+
+
+def main():
+    myTurtle = turtle.Turtle()
+    # drawPolygon(myTurtle, 5) #draws a pentagon
+    # drawPolygon(myTurtle, 8) #draws an octogon
+
+    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+
+    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+
+
+main()
+input("Hit enter to quit") #this keeps the turtle window from disapearing too soon.
